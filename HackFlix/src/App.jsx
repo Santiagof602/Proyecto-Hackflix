becc2857059ed883941a937ec2d1a8c8971093a6
Santif602 <santiago.fernandez@anima.edu.uy>
2025-07-03 17:27:22 -0300

    Commit casi definitivo
    es por acá

--- HackFlix/src/App.jsx
+++ HackFlix/src/App.jsx
@@ -1,24 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
 import MovieGallery from "./components/MovieGallery";
 import MovieDetails from "./components/MovieDetails";
+import SearchPage from "./components/SearchPage"; //
 import NotFound from "./components/NotFound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <MovieGallery />
-            </>
-          }
-        />
+        <Route path="/" element={<MovieGallery />} />
+        <Route path="/buscar" element={<SearchPage />} />
         <Route path="/pelicula/:id" element={<MovieDetails />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- HackFlix/src/components/Navbar.jsx
+++ HackFlix/src/components/Navbar.jsx
@@ -1,37 +1,42 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
 
-export default function Navbar() {
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 50);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
+function NavBar() {
   return (
-    <nav
-      className={`navbar fixed-top px-5 py-3 ${
-        scrolled ? "navbar-dark bg-black" : "navbar-transparent"
-      }`}
-    >
-      <span className="navbar-brand text-white fs-4">Hackflix</span>
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" fill="currentColor" class="bi bi-camera-reels" viewBox="0 0 16 16">
-        <path d="M6 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0M1 3a2 2 0 1 0 4 0 2 2 0 0 0-4 0"/>
-        <path d="M9 6h.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 7.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 16H2a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2zm6 8.73V7.27l-3.5 1.555v4.35zM1 8v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1"/>
-        <path d="M9 6a3 3 0 1 0 0-6 3 3 0 0 0 0 6M7 3a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
-        </svg>
-      <Link to="/" className="nav-link text-white ms-auto">
-        Home
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="12" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
-        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
-        </svg>
-      </Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          HackFlix
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Inicio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/buscar">
+                Buscar
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
+
+export default NavBar;
