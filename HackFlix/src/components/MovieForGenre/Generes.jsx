873ee6e5ed4cef6bc6023c96eb645c046ffc5b51
MarceEscobal <marcelo.escobal@anima.edu.uy>
2025-07-06 05:26:15 -0300

    Limpiar Buscador al cambiar Genero

--- HackFlix/src/components/MovieForGenre/Generes.jsx
+++ HackFlix/src/components/MovieForGenre/Generes.jsx
@@ -2,9 +2,16 @@
 import { useNavigate } from "react-router-dom";
 import "./Genres.css";
 import { GENRES } from "../../consts/genres";
+import { useSearch } from "../Search/SearchContext.jsx";
 
 export default function Genres() {
   const navigate = useNavigate();
+  const { setSearchTerm } = useSearch();
+
+  const handleGenreClick = (genreId) => {
+    navigate(`/genero/${genreId}`);
+    setSearchTerm("");
+  };
 
   return (
     <div className="genres-grid">
@@ -13,7 +20,7 @@ export default function Genres() {
           key={genre.id}
           className="genre-card"
           style={{ backgroundImage: `url(${genre.img})` }}
-          onClick={() => navigate(`/genero/${genre.id}`)}
+          onClick={() => handleGenreClick(genre.id)}
         >
           <div className="genre-overlay">
             <h2>{genre.name}</h2>
